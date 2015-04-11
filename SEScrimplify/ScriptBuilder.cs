4d631a56f909d46ebe79ce7472eaa01308b27039
Alex Davidson <alex.davidson@bluewire-technologies.com>
2015-04-11 12:44:32 +0100

    Factor out script parsing.
    Support parsing multiple source files into the same workspace. Compile as
    a library to eliminate the need for an entry point function.

---
+++ SEScrimplify/ScriptBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SEScrimplify
+{
+    /// <summary>
+    /// Combines multiple scripts into a single valid SyntaxTree which can be compiled as a C# program.
+    /// </summary>
+    public class ScriptBuilder
+    {
+        readonly List<CompilationUnitSyntax> units = new List<CompilationUnitSyntax>();
+
+        private CompilationUnitSyntax ParseScriptUnit(string script)
+        {
+            var node = SyntaxFactory.ParseCompilationUnit(script);
+            var syntaxDiagnostics = units.SelectMany(u => u.GetDiagnostics()).Where(d => d.WarningLevel <= 0).ToArray();
+            if (syntaxDiagnostics.Any()) throw new SyntaxDiagnosticFailureException(node.SyntaxTree, syntaxDiagnostics.ToArray());
+            return node;
+        }
+
+        public void AddScript(string content)
+        {
+            units.Add(ParseScriptUnit(content));
+        }
+
+        public SyntaxTree ToSyntaxTree()
+        {
+            // Wrap bare script members in a __Script__ class. Permits compilation as a C# library.
+            var tree = SyntaxFactory.SyntaxTree(
+                SyntaxFactory.CompilationUnit(
+                    SyntaxFactory.List(units.SelectMany(u => u.Externs).Distinct()),
+                    SyntaxFactory.List(units.SelectMany(u => u.Usings).Distinct()),
+                    SyntaxFactory.List<AttributeListSyntax>(),
+                    SyntaxFactory.List<MemberDeclarationSyntax>(
+                        new[] {
+                            SyntaxFactory.ClassDeclaration("__Script__").WithMembers(
+                                SyntaxFactory.List(units.SelectMany(u => u.Members))).NormalizeWhitespace()
+                        })));
+
+            var mainMethod = units.SelectMany(u => u.Members).OfType<MethodDeclarationSyntax>().Where(m => m.Identifier.Text == "Main").ToList();
+            if (!mainMethod.Any()) throw new SyntaxDiagnosticFailureException(tree, "No Main() method has been defined.");
+            if (!mainMethod.Any(m => m.Arity == 0)) throw new SyntaxDiagnosticFailureException(tree, "Main() method must not take any arguments.");
+
+            return tree;
+        }
+    }
+}
--- SEScrimplify/ScriptParser.cs
+++ SEScrimplify/ScriptParser.cs
@@ -13,52 +13,23 @@ namespace SEScrimplify
         /// Parses a Space Engineers script into a valid C# syntax tree.
         /// </summary>
         /// <remarks>
-        /// The resulting tree will contain some additional boilerplate in order to make it compile as a C# program.
+        /// The resulting tree will contain some additional boilerplate in order to make it compile as a C# library.
         /// </remarks>
         /// <param name="script"></param>
         /// <returns></returns>
         public SyntaxTree ParseScript(string script)
         {
-            var node = SyntaxFactory.ParseCompilationUnit(script);
-
-            var syntaxDiagnostics = node.GetDiagnostics().Where(d => d.WarningLevel <= 0).ToArray();
-            if (syntaxDiagnostics.Any()) throw new SyntaxDiagnosticFailureException(node.SyntaxTree, syntaxDiagnostics.ToArray());
-
-            return CreateValidTree(node);
-        }
-
-        /// <summary>
-        /// Wraps bare script members in a __Script class containing a static Main() method.
-        /// Permits compilation as a C# program.
-        /// </summary>
-        /// <param name="unit"></param>
-        /// <returns></returns>
-        private static SyntaxTree CreateValidTree(CompilationUnitSyntax unit)
-        {
-            var fakeEntryPoint = SyntaxFactory.ParseCompilationUnit("static void Main(string[] args) { new __Script__().Main(); }");
-
-            var mainMethod = unit.Members.OfType<MethodDeclarationSyntax>().Where(m => m.Identifier.Text == "Main").ToList();
-            if (!mainMethod.Any()) throw new SyntaxDiagnosticFailureException(unit.SyntaxTree, "No Main() method has been defined.");
-            if (!mainMethod.Any(m => m.Arity == 0)) throw new SyntaxDiagnosticFailureException(unit.SyntaxTree, "Main() method must not take any arguments.");
-
-            return SyntaxFactory.SyntaxTree(
-                SyntaxFactory.CompilationUnit(
-                    unit.Externs,
-                    unit.Usings,
-                    SyntaxFactory.List<AttributeListSyntax>(),
-                    SyntaxFactory.List<MemberDeclarationSyntax>(
-                        new[] {
-                            SyntaxFactory.ClassDeclaration("__Script__").WithMembers(
-                                SyntaxFactory.List(fakeEntryPoint.Members.Concat(unit.Members))).NormalizeWhitespace()
-                        })));
-
+            var builder = new ScriptBuilder();
+            builder.AddScript(script);
+            return builder.ToSyntaxTree();
         }
 
         public CSharpCompilation Compile(SyntaxTree tree)
         {
             return CSharpCompilation.Create("Internal").AddSyntaxTrees(tree)
                 .AddReferences(MetadataReference.CreateFromAssembly(typeof(Object).Assembly)) // Add reference mscorlib.dll
-                .AddReferences(MetadataReference.CreateFromAssembly(typeof(Enumerable).Assembly)); // Add reference System.Core.dll;
+                .AddReferences(MetadataReference.CreateFromAssembly(typeof(Enumerable).Assembly)) // Add reference System.Core.dll;
+                .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, scriptClassName: "__Script__"));
         }
 
         public CSharpCompilation StrictCompile(SyntaxTree tree)
@@ -66,7 +37,7 @@ namespace SEScrimplify
             var compilation = Compile(tree);
             var compilationDiagnostics = compilation.GetDiagnostics().Where(d => d.WarningLevel <= 0).ToArray();
             if (compilationDiagnostics.Any()) throw new CompilationDiagnosticFailureException(compilation, compilationDiagnostics.ToArray());
-
+            
             return compilation;
         }
 
