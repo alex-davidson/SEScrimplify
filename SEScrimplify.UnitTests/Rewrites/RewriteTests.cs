d2080c48e500d0213075dce702164c380b702e9e
Alex Davidson <alex.davidson@bluewire-technologies.com>
2015-04-16 21:37:23 +0100

    Refactor tests.
    Unify syntax rewrite tests as a single fixture. Provide helper methods.

---
+++ SEScrimplify.UnitTests/Rewrites/RewriteTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using SEScrimplify.Rewrites;
+
+namespace SEScrimplify.UnitTests.Rewrites
+{
+    public class RewriteTests : RewriteTestsBase
+    {
+        [TestCase("NoExternalReferences")]
+        [TestCase("MultipleArguments")]
+        [TestCase("ReferenceToMethodLocalVariable")]
+        [TestCase("ReferenceToFieldOfClass")]
+        [TestCase("ReferenceToPropertyOfClass")]
+        public void LambdaAsMemberFunctionRewrite(string scriptName)
+        {
+            ExpectEquivalenceAfterRewrite(new LambdaAsMemberFunctionRewrite(new GeneratedMemberNameProvider(0)), "Rewrites.Lambda", scriptName);
+        }
+
+        [TestCase("ExtensionCall", "StaticCall")]
+        [TestCase("StaticCall", "StaticCall", Description = "Existing static calls should be left unchanged.")]
+        public void ExtensionMethodCallAsStaticMethodCallRewrite(string originalScriptName, string rewrittenScriptName)
+        {
+            ExpectEquivalenceAfterRewrite(new ExtensionMethodCallAsStaticMethodCallRewrite(), "Rewrites.ExtensionMethod", originalScriptName, rewrittenScriptName);
+        }
+    }
+}
---
+++ SEScrimplify.UnitTests/Rewrites/RewriteTestsBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using NUnit.Framework;
+using SEScrimplify.Util;
+
+namespace SEScrimplify.UnitTests.Rewrites
+{
+    public abstract class RewriteTestsBase
+    {
+        protected ScriptParser Parser = new ScriptParser();
+
+        protected SyntaxTree LoadScript(string name)
+        {
+            var script = Parser.ParseScript(EmbeddedResources.GetScript(name));
+            Parser.StrictCompile(script);
+            return script;
+        }
+        protected void ExpectEquivalenceAfterRewrite(ISemanticallyAwareRewrite rewrite, string scriptRoot, string scriptName)
+        {
+            ExpectEquivalenceAfterRewrite(rewrite, scriptRoot, scriptName, String.Format("{0}.Rewritten", scriptName));
+        }
+
+        protected void ExpectEquivalenceAfterRewrite(ISemanticallyAwareRewrite rewrite, string scriptRoot, string originalScriptName, string rewrittenScriptName)
+        {
+            var tree = LoadScript(String.Format("{0}.{1}.txt", scriptRoot, originalScriptName));
+            var expectedOutput = LoadScript(String.Format("{0}.{1}.txt", scriptRoot, rewrittenScriptName));
+
+            var rewritten = RewriteScript(tree, rewrite);
+
+            Assert.That(rewritten, Is.EqualTo(expectedOutput).Using<SyntaxTree>(new SyntaxEquivalenceComparer()));
+        }
+
+        protected SyntaxTree RewriteScript(SyntaxTree tree, ISemanticallyAwareRewrite rewrite)
+        {
+            var compilation = Parser.StrictCompile(tree);
+            var rewrittenSyntax = SyntaxFactory.SyntaxTree(rewrite.Rewrite(tree.GetRoot(), Parser.GetSemanticModel(compilation, tree)).NormalizeWhitespace());
+            Parser.StrictCompile(rewrittenSyntax);
+            return rewrittenSyntax;
+        }
+    }
+}
